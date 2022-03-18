761cb8e185f8610c3e97f9da95ce169193a13c3c
Manuel Thalmann <m@nuth.ch>
2022-03-18 18:11:46 +0100

    Refactor the debug output script

---
+++ Assets/Scripts/DebugOutput.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Provides the functionality to handle debug output.
+/// </summary>
+public class DebugOutput : MonoBehaviour
+{
+    /// <summary>
+    /// The player to get the debug information from.
+    /// </summary>
+    [SerializeField]
+    private GameObject player = null;
+
+    /// <summary>
+    /// Gets the text object to print the debug text to.
+    /// </summary>
+    protected Text DebugText
+    {
+        get
+        {
+            return GetComponent<Text>();
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the player to get the debug information from.
+    /// </summary>
+    public GameObject Player => player;
+
+    /// <summary>
+    /// Gets the movement of the player to get debug information from.
+    /// </summary>
+    protected PlayerMovement Movement
+    {
+        get
+        {
+            return Player.GetComponent<PlayerMovement>();
+        }
+    }
+
+    /// <summary>
+    /// Gets the controller of the player to get debug information from.
+    /// </summary>
+    protected PlayerController Controller
+    {
+        get
+        {
+            return Player.GetComponent<PlayerController>();
+        }
+    }
+
+    /// <summary>
+    /// Gets the rigidbody of the player to get debug information from.
+    /// </summary>
+    protected Rigidbody2D Rigidbody
+    {
+        get
+        {
+            return Player.GetComponent<Rigidbody2D>();
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the elapsed time.
+    /// </summary>
+    protected float ElapsedTime { get; set; }
+
+    /// <summary>
+    /// Gets or sets the state of the jump action.
+    /// </summary>
+    protected string JumpState { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Movement.jumpAction.performed +=
+            context =>
+            {
+                JumpState = context.phase.ToString();
+            };
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ElapsedTime += Time.deltaTime;
+        DebugText.text = "";
+        DebugText.text += "Grounded: " + Controller.m_Grounded.ToString();
+        DebugText.text += "\nJump: " + JumpState;
+        DebugText.text += "\nTime: " + ElapsedTime.ToString();
+        DebugText.text += "\nH-Spd: " + Rigidbody.velocity.x;
+        DebugText.text += "\nV-Spd: " + Rigidbody.velocity.y;
+    }
+}
