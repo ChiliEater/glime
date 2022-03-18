94ff1e4edd88226a93c1d76d8e2e67f9fb794f3c
Manuel Thalmann <m@nuth.ch>
2022-03-18 17:37:40 +0100

    Refactor the `FollowPlayer` class

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,18 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Provides the functionality to let a target follow the player.
+/// </summary>
 public class FollowPlayer : MonoBehaviour
 {
-    public Transform target;
-    public float smoothSpeed;
-    public Vector3 offset;
+    /// summary>
+    /// Gets or sets the transformation target.
+    /// </summary>
+    [SerializeField]
+    public Transform Target { get; set; }
 
-    // Update is called once per frame
+    /// summary>
+    /// Gets or sets the speed of the target.
+    /// </summary>
+    [SerializeField]
+    public float SmoothSpeed { get; set; }
+
+    /// <summary>
+    /// Gets or sets the offset of the target in relation to the player.
+    /// </summary>
+    [SerializeField]
+    public Vector3 Offset { get; set; }
+
+    /// <summary>
+    /// Handles updates on each frame. 
+    /// </summary>
     void FixedUpdate()
     {
-
-        Vector3 desiredPosition = target.position + offset;
-		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-		transform.position = smoothedPosition;
+        Vector3 desiredPosition = Target.position + Offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed);
+        transform.position = smoothedPosition;
     }
 }
