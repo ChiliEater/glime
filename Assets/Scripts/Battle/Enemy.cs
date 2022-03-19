41040cb0af60b1e40ce8dc6d3ae73a06161a735b
sebastian.zumbrunn <sebastian.zumbrunn@pm.me>
2022-03-19 23:11:12 +0100

    add enemy ai

--- Assets/Scripts/Battle/Enemy.cs
+++ Assets/Scripts/Battle/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace CodeBrewery.Glime.Battle
 {
@@ -19,12 +20,47 @@ namespace CodeBrewery.Glime.Battle
         /// </summary>
         public List<EnemyType> Type { get; } = new List<EnemyType>();
 
+        private NavMeshAgent NavMeshAgent { get; set; }
+
+        public Vector3 Target { get; private set; }
+
+        public float Speed { get; set; }
+
+        public float MaxDistance { get; set; }
+
+        public float CurrentDistance { get; private set; }
+
+        private Vector3 lastPosition;
+
         /// <summary>
         /// Initializes the enemy.
         /// </summary>
         public void Start()
         {
             Type.AddRange(type);
+            NavMeshAgent = GetComponent<NavMeshAgent>();
+            lastPosition = transform.position;
+        }
+
+        void Update()
+        {
+            if (!NavMeshAgent.isStopped)
+            {
+                CurrentDistance += Vector3.Distance(lastPosition, transform.position);
+                lastPosition = transform.position;
+                if (CurrentDistance > MaxDistance)
+                {
+                    NavMeshAgent.isStopped = true;
+                }
+            }
+        }
+
+        public void TurnStarts(EncounterManager encounterManager)
+        {
+            CurrentDistance = 0;
+            lastPosition = transform.position;
+            Target = encounterManager.EnemyTarget;
+            NavMeshAgent.isStopped = false;
         }
     }
 }
