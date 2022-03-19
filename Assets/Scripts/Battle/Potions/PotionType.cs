e3d30174eb960672f7cc589802befebb7082cf2d
Manuel Thalmann <m@nuth.ch>
2022-03-19 16:46:28 +0100

    Add enum for representing potion types

---
+++ Assets/Scripts/Battle/Potions/PotionType.cs
@@ -0,0 +1,38 @@
+namespace CodeBrewery.Glime.Battle
+{
+    /// <summary>
+    /// Represents the type of a potion.
+    /// </summary>
+    public enum PotionType
+    {
+        /// <summary>
+        /// Indicates the fire type.
+        /// </summary>
+        Fire,
+
+        /// <summary>
+        /// Indicates the ice type.
+        /// </summary>
+        Ice,
+
+        /// <summary>
+        /// Indicates the electric type.
+        /// </summary>
+        Electric,
+
+        /// <summary>
+        /// Indicates the healing type.
+        /// </summary>
+        Healing,
+        
+        /// <summary>
+        /// Indicates the strength type.
+        /// </summary>
+        Strength,
+        
+        /// <summary>
+        /// Indicates the weakness type.
+        /// </summary>
+        Weakness
+    }
+}
