2eae573266784abe4dcce85919d266d7f1310427
costajon <costajon@students.zhaw.ch>
2022-03-19 18:12:51 +0100

    Implement Custom Button

---
+++ Assets/Scripts/CustomButton.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.OnScreen;
+using UnityEngine.UI;
+
+public class CustomButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+{
+    /// <summary>
+    /// Flag to check if mouse has already left the button
+    /// </summary>
+    private bool mouseOverButton = false;
+
+    /// <summary>
+    /// The sprite renderer that is controlled.
+    /// </summary>
+    private SpriteRenderer spriteRenderer;
+
+    /// <summary>
+    /// Base sprite
+    /// </summary>
+    private Sprite normalSprite;
+
+    /// <summary>
+    /// Hover sprite
+    /// </summary>
+    [SerializeField]
+    private Sprite hoverSprite;
+
+    /// <summary>
+    /// Down sprite
+    /// </summary>
+    [SerializeField]
+    private Sprite downSprite;
+
+
+    /// <summary>
+    /// An event which is triggered on click release.
+    /// </summary>
+    [Header("Events")]
+    [Space]
+    public UnityEvent onClickEvent;
+
+    public void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        normalSprite = spriteRenderer.sprite;
+
+        if (hoverSprite == null)
+        {
+            hoverSprite = normalSprite;
+        }
+
+        if (downSprite == null)
+        {
+            downSprite = hoverSprite;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseOverButton = true;
+        spriteRenderer.sprite = hoverSprite;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        spriteRenderer.sprite = downSprite;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (mouseOverButton)
+        {
+            // Only invoke when the mouse is still hovering over the button
+            spriteRenderer.sprite = hoverSprite;
+            onClickEvent.Invoke();
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseOverButton = false;
+        spriteRenderer.sprite = normalSprite;
+    }
+
+}
