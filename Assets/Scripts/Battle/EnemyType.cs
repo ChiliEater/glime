7843452809cd83a1ead4980bf2a3279c7f8f7f97
Manuel Thalmann <m@nuth.ch>
2022-03-19 16:42:03 +0100

    Add an enum for representing enemy-types

---
+++ Assets/Scripts/Battle/EnemyType.cs
@@ -0,0 +1,38 @@
+namespace CodeBrewery.Glime.Battle
+{
+    /// <summary>
+    /// Represents the type of an enemy.
+    /// </summary>
+    public enum EnemyType
+    {
+        /// <summary>
+        /// Indicates the ice type.
+        /// </summary>
+        Ice,
+
+        /// <summary>
+        /// Indicates the tank type.
+        /// </summary>
+        Tank,
+
+        /// <summary>
+        /// Indicates the agile type.
+        /// </summary>
+        Agile,
+
+        /// <summary>
+        /// Indicates the undead type.
+        /// </summary>
+        Undead,
+
+        /// <summary>
+        /// Indicates the hellish type.
+        /// </summary>
+        Hellish,
+
+        /// <summary>
+        /// Indicates the living type.
+        /// </summary>
+        Living
+    }
+}
