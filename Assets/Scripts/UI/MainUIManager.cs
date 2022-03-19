548445ff99e61db0aec326cb1ed59ae7fff2fc3d
sebastian.zumbrunn <sebastian.zumbrunn@pm.me>
2022-03-20 00:47:42 +0100

    add animator to prefabs

--- Assets/Scripts/UI/MainUIManager.cs
+++ Assets/Scripts/UI/MainUIManager.cs
@@ -7,41 +7,26 @@ using UnityEngine.UIElements;
 namespace CodeBrewery.Glime
 {
 
-    [RequireComponent(typeof(UIDocument))]
     public class MainUIManager : MonoBehaviour
     {
-        private UIDocument uIDocument;
-        private VisualElement craftingUI;
-        private VisualElement battleUI;
+        public GameObject CraftingUI;
+        public GameObject CombatUI;
 
         void Start()
         {
-            uIDocument = GetComponent<UIDocument>();
-            VisualElement rootEl = this.uIDocument.rootVisualElement;
-            craftingUI = rootEl.Q("PosionUI");
-            battleUI = rootEl.Q("BattleUI");
-
-            rootEl.Q<Button>("CraftButton").clickable.clicked += SwitchToCraftingUI;
-            rootEl.Q<Button>("BattleButton").clickable.clicked += SwitchToBattleUI;
-
-            SwitchToCraftingUI();
         }
 
-        void Update()
-        {
 
+        public void SwitchToCraftingUI()
+        {
+            CraftingUI.SetActive(true);
+            CombatUI.SetActive(false);
         }
 
-        void SwitchToCraftingUI()
+        public void SwitchToBattleUI()
         {
-            craftingUI.style.display = DisplayStyle.Flex;
-            battleUI.style.display = DisplayStyle.None;
-        }
-
-        void SwitchToBattleUI()
-        {
-            battleUI.style.display = DisplayStyle.Flex;
-            craftingUI.style.display = DisplayStyle.None;
+            CraftingUI.SetActive(false);
+            CombatUI.SetActive(true);
         }
 
     }
