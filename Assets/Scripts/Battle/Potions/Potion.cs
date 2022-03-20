291fb538538bade063e07aff765a39fef959dbb8
Manuel Thalmann <m@nuth.ch>
2022-03-20 11:57:13 +0100

    Rename properties

--- Assets/Scripts/Battle/Potions/Potion.cs
+++ Assets/Scripts/Battle/Potions/Potion.cs
@@ -2,11 +2,11 @@ namespace CodeBrewery.Glime.Battle.Potions
 {
     public class Potion
     {
-        public PotionTypeSet potionTypes { get; private set; }
+        public PotionTypeSet PotionTypes { get; private set; }
 
         public Potion(PotionTypeSet potionTypes)
         {
-            this.potionTypes = potionTypes;
+            this.PotionTypes = potionTypes;
         }
     }
 }
