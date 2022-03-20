8d0c535e2b8aba72e8f818c52f64c84bd01dce3f
Manuel Thalmann <m@nuth.ch>
2022-03-20 14:31:43 +0100

    Make the `Potion`s types readonly

--- Assets/Scripts/Battle/Potions/Potion.cs
+++ Assets/Scripts/Battle/Potions/Potion.cs
@@ -8,13 +8,13 @@ namespace CodeBrewery.Glime.Battle.Potions
         /// <summary>
         /// Gets the type of the potion.
         /// </summary>
-        public PotionTypeSet PotionTypes { get; private set; }
+        public ReadonlyPotionTypeSet PotionTypes { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Potion"/> class.
         /// </summary>
         /// <param name="potionTypes">The type of the potion.</param>
-        public Potion(PotionTypeSet potionTypes)
+        public Potion(ReadonlyPotionTypeSet potionTypes)
         {
             this.PotionTypes = potionTypes;
         }
