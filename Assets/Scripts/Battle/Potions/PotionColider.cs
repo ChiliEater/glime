0a49aa8c5c13d45436d6106e124fe7379e109d91
sebastian.zumbrunn <sebastian.zumbrunn@pm.me>
2022-03-20 23:10:47 +0100

    add pointer spawner

--- Assets/Scripts/Battle/Potions/PotionColider.cs
+++ Assets/Scripts/Battle/Potions/PotionColider.cs
@@ -9,6 +9,10 @@ namespace CodeBrewery.Glime.Battle.Potions
     [RequireComponent(typeof(Collider2D))]
     public class PotionColider : MonoBehaviour
     {
+        private static readonly int BASE_DAMAGE = 2;
+        private static readonly int DAMAGE_MODIFIER = 3;
+        private static readonly int ADDED_SPEED = 3;
+
         private static readonly float POTION_EFFECT_DURATION = 3;
         public Potion potion { get; set; }
         private float spawnedAt;
@@ -31,8 +35,61 @@ namespace CodeBrewery.Glime.Battle.Potions
             Enemy enemy = collision.gameObject.GetComponent<Enemy>();
             if (enemy != null)
             {
-
+                ApplyDamage(enemy, potion);
             }
         }
+
+        private void ApplyDamage(Enemy enemy, Potion potion)
+        {
+            int damage = 0;
+            int attack = 0;
+            List<EnemyType> enemyTypes = enemy.Type;
+            foreach(KeyValuePair<PotionType, int> keyValue in potion.PotionTypes)
+            {
+                PotionType potionType = keyValue.Key;
+                int potionCount = keyValue.Value;
+                if(potionType == PotionType.Fire)
+                {
+                    damage += GetIceFireDamageType(EnemyType.Ice, EnemyType.Hellish, enemyTypes)*potionCount;
+                }
+                else if(potionType == PotionType.Ice)
+                {
+                    damage += GetIceFireDamageType(EnemyType.Hellish, EnemyType.Ice, enemyTypes)*potionCount;
+                } else if(potionType == PotionType.Healing)
+                {
+                    if (enemyTypes.Contains(EnemyType.Undead))
+                    {
+                        damage += BASE_DAMAGE*potionCount;
+                    }
+                    else
+                    {
+                        damage -= BASE_DAMAGE*potionCount;
+                    }
+                } else if(potionType == PotionType.Strength)
+                {
+                    if(enemyTypes.Contains(EnemyType.Undead))
+                    {
+                        attack += BASE_DAMAGE*potionCount;
+                    }
+                }
+            }
+            enemy.HitPoints -= damage;
+            enemy.Attack = Mathf.Min(0, enemy.Attack - attack);
+        }
+
+        private int GetIceFireDamageType(EnemyType advantageType, EnemyType disadvantageType, List<EnemyType> enemyTypes)
+        {
+
+            int damage = BASE_DAMAGE;
+            if(enemyTypes.Contains(advantageType) )
+            {
+                damage += DAMAGE_MODIFIER; 
+            } else if(enemyTypes.Contains(disadvantageType))
+            {
+                damage -= DAMAGE_MODIFIER; 
+            }
+            return damage;
+        }
+
     }
 }
--- Assets/Scripts/Battle/Potions/PotionSpawner.cs
+++ Assets/Scripts/Battle/Potions/PotionSpawner.cs
@@ -1,16 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace CodeBrewery.Glime.Battle.Potions
 {
     [RequireComponent(typeof(Collider2D))]
-    public class PotionSpawner : MonoBehaviour
+    public class PotionSpawner : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
     {
-        private void OnMouseDown()
+        public GameObject PotionCollider;
+        private void Update()
         {
-            Vector2 mousePos = Input.mousePosition;
+            /*if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("test");
+                Vector2 mousePos = Input.mousePosition;
+                Instantiate(PotionCollider, mousePos, Quaternion.identity);
+            }*/
+        }
 
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            Debug.Log("test up");
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Debug.Log("test down");
         }
     }
 }
